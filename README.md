c9e99307954acfc60cadc3098947b4a98d364a50
phuongnq <nquangphuong@gmail.com>
2021-03-24 12:54:48 +0700

    Update plugin

--- README.md
+++ README.md
@@ -5,6 +5,15 @@ A site plugin for CrafterCMS to integrete Youtube Video
 
 ## Setup
 
+### Copy control file to plugin
+
+```
+mkdir -p ${SITE}/sandbox/config/studio/plugins/control/youtubepicker/ # If directory is not created
+cp authoring/control/youtubepicker/main.js ${SITE}/sandbox/config/studio/plugins/control/youtubepicker/main.js
+```
+
+Then commit
+
 ### Update site-config-tool.xml
 
 ```
@@ -24,4 +33,14 @@ vim ${SITE}/sandbox/config/studio/administration/site-config-tools.xml
 ...
 </controls>
 ```
+
 Then commit
+
+Or via UI
+
+
+### Update Google API Key
+
+https://console.cloud.google.com/apis/dashboard
+
+Sample: `AIzaSyBAQK6l_uH5cYPSMRrU9kZUP0cfJjKc3Cs`
--- src/main.js
+++ src/main.js
@@ -1,12 +1,9 @@
-"use strict";
-
 (function () {
   var React = CrafterCMSNext.React;
   var ReactDOM = CrafterCMSNext.ReactDOM;
 
   async function searchYouTube(keyword, googleApiKey) {
-    const url = `https://youtube.googleapis.com/youtube/v3/search?part=snippet&type=video&q=${keyword}&key=${googleApiKey}`;
-
+    const url = `https://youtube.googleapis.com/youtube/v3/search?part=snippet&type=video&q=${keyword}&key=${googleApiKey}`
     try {
       const response = await fetch(url);
       const data = await response.json();
@@ -16,103 +13,98 @@
     }
   }
 
-  function SearchBar({
-    onSearchSubmit
-  }) {
+  function SearchBar({ onSearchSubmit }) {
     const [keyword, setKeyword] = React.useState('');
 
-    const searchChange = e => {
+    const searchChange = (e) => {
       setKeyword(e.target.value);
     };
 
-    const submitSearch = e => {
+    const submitSearch = (e) => {
       e.preventDefault();
       onSearchSubmit(keyword);
     };
 
-    return /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("form", {
-      onSubmit: submitSearch,
-      style: {
-        marginTop: '20px'
-      }
-    }, /*#__PURE__*/React.createElement("input", {
-      type: "text",
-      placeholder: "Search Youtube",
-      className: "form-control",
-      onChange: searchChange
-    })));
+    return (
+      <div>
+          <form onSubmit={submitSearch} style={{marginTop:'20px'}}>
+            <input
+              type="text"
+              placeholder="Search Youtube"
+              className="form-control"
+              onChange={searchChange}
+            />
+          </form>
+        </div>
+    );
   }
 
-  function VideoList({
-    videos,
-    onVideoSelect
-  }) {
-    const list = videos.map(video => /*#__PURE__*/React.createElement(VideoListItem, {
-      onVideoSelect: onVideoSelect,
-      key: video.etag,
-      video: video
-    }));
-    return /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("ul", {
-      className: "col-md-4 list-group",
-      style: {
-        marginTop: '20px'
-      }
-    }, list));
+  function VideoList({ videos, onVideoSelect }) {
+    const list = videos.map((video) =>
+      <VideoListItem
+        onVideoSelect={onVideoSelect}
+        key={video.etag}
+        video={video}
+      />
+    );
+    return (
+      <div>
+        <ul className="col-md-4 list-group" style={{marginTop:'20px'}}>
+          {list}
+        </ul>
+      </div>
+    )
   }
 
-  function VideoListItem({
-    video,
-    onVideoSelect
-  }) {
+  function VideoListItem({ video, onVideoSelect }) {
     const imgUrl = video.snippet.thumbnails.default.url;
-    return /*#__PURE__*/React.createElement("li", {
-      className: "list-group-item",
-      onClick: () => onVideoSelect(video)
-    }, /*#__PURE__*/React.createElement("div", {
-      className: "video-list-media"
-    }, /*#__PURE__*/React.createElement("div", {
-      className: "media-left"
-    }, /*#__PURE__*/React.createElement("img", {
-      className: "media-object",
-      src: imgUrl
-    })), /*#__PURE__*/React.createElement("div", {
-      className: "media-body"
-    }, /*#__PURE__*/React.createElement("div", {
-      className: "media-heading"
-    }, /*#__PURE__*/React.createElement("div", null, video.snippet.title)))));
+    return (
+      <li className="list-group-item"  onClick={() => onVideoSelect(video)}>
+        <div className="video-list-media">
+          <div className="media-left">
+            <img className="media-object" src={imgUrl} />
+          </div>
+
+          <div className="media-body">
+            <div className="media-heading">
+              <div>{video.snippet.title}</div>
+            </div>
+          </div>
+
+        </div>
+      </li>
+    );
   }
 
-  function VideoDetail({
-    video
-  }) {
+  function VideoDetail({ video }) {
     if (!video) {
-      return /*#__PURE__*/React.createElement("div", null);
+      return(
+        <div>
+        </div>
+      )
     }
 
     const videoId = video.id.videoId;
     const url = `https://youtube.com/embed/${videoId}`;
-    return /*#__PURE__*/React.createElement("div", {
-      className: "video-detail col-md-8"
-    }, /*#__PURE__*/React.createElement("div", {
-      className: "embed-responsive embed-responsive-16by9",
-      style: {
-        marginTop: '20px'
-      }
-    }, /*#__PURE__*/React.createElement("iframe", {
-      className: "embed-responsive-item",
-      src: url
-    })), /*#__PURE__*/React.createElement("div", {
-      className: "details"
-    }, /*#__PURE__*/React.createElement("div", null, video.snippet.title), /*#__PURE__*/React.createElement("div", null, video.snippet.description)));
+
+    return (
+      <div className="video-detail col-md-8">
+        <div className="embed-responsive embed-responsive-16by9" style={{marginTop:'20px'}}>
+          <iframe className="embed-responsive-item" src={url}></iframe>
+        </div>
+        <div className="details">
+          <div>{video.snippet.title}</div>
+          <div>{video.snippet.description}</div>
+        </div>
+      </div>
+    )
   }
 
-  function MyPicker({
-    googleApiKey
-  }) {
+  function MyPicker({ googleApiKey }) {
     const [selectedVideo, setSelectedVideo] = React.useState(null);
     const [videos, setVideos] = React.useState([]);
 
-    const videoSearch = async keyword => {
+    const videoSearch = async (keyword) => {
       const res = await searchYouTube(keyword, googleApiKey);
 
       if (res && res.items && res.items.length >= 0) {
@@ -123,34 +115,43 @@
       }
     };
 
-    const onSelectVideo = video => {
+    const onSelectVideo = (video) => {
       setSelectedVideo(video);
       updateInputs(video);
     };
 
-    const updateInputs = video => {
+    const updateInputs = (video) => {
       if (typeof $ !== 'function') return;
+
       $('#youtubeID_s').find('input')[0].focus();
       $('#youtubeID_s').find('input')[0].value = video.id.videoId;
+
       $('#title_s').find('input')[0].focus();
       $('#title_s').find('input')[0].value = video.snippet.title;
+
       $('#description_t').find('textarea')[0].focus();
       $('#description_t').find('textarea')[0].value = video.snippet.description;
+
       $('#posterImage_s').find('input')[0].focus();
       $('#posterImage_s').find('input')[0].value = video.snippet.thumbnails.high.url;
-    };
+    }
 
-    return /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("h4", null, "Youtube Picker"), /*#__PURE__*/React.createElement(SearchBar, {
-      onSearchSubmit: keyword => videoSearch(keyword)
-    }), /*#__PURE__*/React.createElement(VideoDetail, {
-      video: selectedVideo
-    }), /*#__PURE__*/React.createElement(VideoList, {
-      onVideoSelect: selectedVideo => onSelectVideo(selectedVideo),
-      videos: videos
-    }));
+    return (
+      <div>
+        <h4>Youtube Picker</h4>
+        <SearchBar onSearchSubmit={(keyword) => videoSearch(keyword)} />
+        <VideoDetail video={selectedVideo}/>
+        <VideoList
+          onVideoSelect={(selectedVideo) => onSelectVideo(selectedVideo)}
+          videos={videos}
+        />
+      </div>
+    );
   }
 
-  CStudioForms.Controls.YoutubePicker = CStudioForms.Controls.YoutubePicker || function (id, form, owner, properties, constraints) {
+  CStudioForms.Controls.YoutubePicker =
+  CStudioForms.Controls.YoutubePicker ||
+  function(id, form, owner, properties, constraints) {
     this.owner = owner;
     this.owner.registerField(this);
     this.errors = [];
@@ -165,18 +166,15 @@
     this.supportedPostFixes = ['_s'];
 
     if (properties) {
-      var required = constraints.find(function (property) {
+      var required = constraints.find(function(property) {
         return property.name === 'required';
       });
-
       if (required) {
         this.required = required.value === 'true';
       }
-
-      var googleapi_key = properties.find(function (property) {
+      var googleapi_key = properties.find(function(property) {
         return property.name === 'googleapi_key';
       });
-
       if (googleapi_key) {
         this.googleapi_key = googleapi_key.value;
       }
@@ -186,41 +184,43 @@
   };
 
   YAHOO.extend(CStudioForms.Controls.YoutubePicker, CStudioForms.CStudioFormField, {
-    getLabel: function () {
+    getLabel: function() {
       return 'Youtube Picker';
     },
-    render: function (config, containerEl) {
+
+    render: function(config, containerEl) {
       // we need to make the general layout of a control inherit from common
       // you should be able to override it -- but most of the time it wil be the same
       containerEl.id = this.id;
       var googleApiKey = this.googleapi_key;
-      ReactDOM.render( /*#__PURE__*/React.createElement(MyPicker, {
-        googleApiKey
-      }), containerEl);
+
+      ReactDOM.render( /*#__PURE__*/React.createElement(MyPicker, { googleApiKey }), containerEl);
     },
-    getValue: function () {
+
+    getValue: function() {
       return this.value;
     },
-    setValue: function (value) {
+
+    setValue: function(value) {
       this.value = value;
     },
-    getName: function () {
+
+    getName: function() {
       return 'youtubepicker';
     },
-    getSupportedProperties: function () {
-      return [{
-        label: 'Google API Key',
-        name: 'googleapi_key',
-        type: 'string',
-        defaultValue: 'AIzaSyBAQK6l_uH5cYPSMRrU9kZUP0cfJjKc3Cs'
-      }];
+
+    getSupportedProperties: function() {
+      return [{ label: 'Google API Key', name: 'googleapi_key', type: 'string', defaultValue: '' }];
     },
-    getSupportedConstraints: function () {
+
+    getSupportedConstraints: function() {
       return [];
     },
-    getSupportedPostFixes: function () {
+
+    getSupportedPostFixes: function() {
       return this.supportedPostFixes;
     }
   });
+
   CStudioAuthoring.Module.moduleLoaded('youtubepicker', CStudioForms.Controls.YoutubePicker);
 })();
